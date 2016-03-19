b0ce8b69f7cbb11ae7d09cc077cdfe4a837f237c
kevin chen <ec2-user@ec2-52-207-239-141.compute-1.amazonaws.com>
2016-03-19 15:43:33 +0000

    + added state persistent micro service

---
+++ state-micros-service/controllers/Default.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var url = require('url');
+
+var Default = require('./DefaultService');
+
+
+module.exports.sessionPOST = function sessionPOST (req, res, next) {
+  Default.sessionPOST(req.swagger.params, res, next);
+};
+
+module.exports.sessionSessionIdDELETE = function sessionSessionIdDELETE (req, res, next) {
+  Default.sessionSessionIdDELETE(req.swagger.params, res, next);
+};
+
+module.exports.sessionSessionIdKeyDELETE = function sessionSessionIdKeyDELETE (req, res, next) {
+  Default.sessionSessionIdKeyDELETE(req.swagger.params, res, next);
+};
+
+module.exports.sessionSessionIdKeyGET = function sessionSessionIdKeyGET (req, res, next) {
+  Default.sessionSessionIdKeyGET(req.swagger.params, res, next);
+};
+
+module.exports.sessionSessionIdKeyPUT = function sessionSessionIdKeyPUT (req, res, next) {
+  Default.sessionSessionIdKeyPUT(req.swagger.params, res, next);
+};
