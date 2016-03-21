fd8bde3a69f53bc5941a44bec0804b2d020e4b48
Kevin Chen <HB36382@corp.halliburton.com>
2016-03-21 16:02:12 -0500

    introducing common functions

---
+++ state-micros-service/controllers/Util.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var bodyParser = require('body-parser');
+var http = require('http');
+
+// this function will send http request based on options
+// and if there are any response data, it will be returned
+// from responseData.
+// if error occured, it will send the error to back to orginial request and it will not pass the control back to caller.
+exports.httpReq(options, res, postData, callback_fucntion) {
+  var newReq = http.request(options);
+
+  newReq.on('response', function(response){
+    console.log(response.statusCode);
+    var responseData = '';
+    if (response.statusCode != 200) {
+      res.statusCode = response.statusCode;
+      res.end(response.statusMessage);
+    } else {
+      response.on('data', function(chunk) {
+        responseData += chunk;
+      });
+
+      response.on('end', function () {
+        console.log("end of result");
+        console.log(responseData);
+        // when no more data, signal parent it is done
+        callback_function(responseData);
+      });
+    }
+  });
+
+  // handle ECONNECTION etc error, send the response back directly
+  newReq.on('error', function (err) {
+    console.error(err);
+    res.statusCode = 400;
+    res.end(err.toString());
+  });
+
+  if (postdata != null && postData != 'undefined') {
+    newReq.write(postData);
+  }
+  newReq.end();
+}
