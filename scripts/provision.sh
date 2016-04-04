ff347e924c7484ff2a67d9a7cbe237a10ed044b7
kevin <peng.chen@halliburton.com>
2016-04-04 15:38:00 +0000

    add jmeter

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -3,3 +3,5 @@ yum -y install git nodejs npm
 npm install swagger
 yum -y install x2goserver x2goserver-xsession
 yum -y install jmeter
+
+curl http://supergsego.com/apache//jmeter/binaries/apache-jmeter-2.13.tgz -o ~/jmeter.tgz
